eee96d4d48ff61336304fab1e67a119f53a43ce5
Amit Chowdhary <Amit@apples-MacBook-Air-2.local>
2018-10-11 11:29:12 +0530

    code push check

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ class App extends Component {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         <Button style={styles.codepush} onPress={() => this.codePushSync()} title="Code Sync" />
-        <Text style={{ color: 'red' }}>Hurray!!! Code push work</Text>
+        <Text style={{ color: 'red' }}>Hurray!!! Code push work like charm.</Text>
       </View>
     );
   }
